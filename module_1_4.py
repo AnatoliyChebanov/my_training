61a2c43cbb86578935ceeeebd9678b0687e898b8
AnatoliyChebanov <av-chebanov@outlook.com>
2024-12-10 23:20:23 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+number_of_tasks=12
+number_of_hours=1.5
+name_course='Python'
+time_for_one_task=(number_of_hours/number_of_tasks)
+print(('Курс:'+(name_course)), ('всего задач:' +(str(number_of_tasks))), ('затрачено часов:' +(str(number_of_hours))), ('среднее время выполнения:' +(str(time_for_one_task))))
