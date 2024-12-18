38cdd3c60326bb8f364ad4c4241aa56cf04d2e84
AnatoliyChebanov <av-chebanov@outlook.com>
2024-12-19 00:21:59 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,14 @@
+first=18
+print(first)
+second=357
+print(second)
+third=759
+print(third)
+if first==second and second==third:
+    print(3)
+if first==third:
+    print(2)
+elif first!=second and second!=third:
+    print(0)
+else:
+    print(2)
